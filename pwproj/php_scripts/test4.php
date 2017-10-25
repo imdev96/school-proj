e8ee13fbb1b309bdc0748fe3f861c372722d4b8c
imdev96 <iuly_m2808@yahoo.com>
2017-10-25 14:25:09 +0300

    Added php scripts for populating players menu from database

---
+++ pwproj/php_scripts/test4.php
@@ -0,0 +1,16 @@
+<?php
+$localhost = 'localhost';
+$user = 'root';
+$pass = 'dbpasswd7';
+$dbname = 'mydb';
+$connect = mysqli_connect("$localhost","$user","$pass","$dbname");
+$idp=4;
+$field='Id_player';
+$query = "select Name from players where $field=$idp";
+$result = mysqli_query($connect, $query);
+
+$row = mysqli_fetch_array($result);
+
+if ($row)
+    echo $row['Name'];
+?>
